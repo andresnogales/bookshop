ce05cd11ff7afc75cf4367c5817598cf2189d37a
Andrés Nogales <alejandroandresnogales@gmail.com>
2021-10-25 23:02:29 -0300

    Project creation

---
+++ src/main/java/com/andresnogales/bookshop/BookshopApplication.java
@@ -0,0 +1,13 @@
+package com.andresnogales.bookshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookshopApplication {
+	//Hola
+	public static void main(String[] args) {		
+		SpringApplication.run(BookshopApplication.class, args);
+	}
+
+}
